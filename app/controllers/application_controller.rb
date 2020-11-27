6584aaab0a6f4f7d2ae1cd4ba6427a05419f02f2
Cloudymolecule <baires82@gmail.com>
2020-11-27 17:35:16 -0500

    finished added and updated on show pages and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -107,12 +107,74 @@ class ApplicationController < Sinatra::Base
         when 4..31
           day = day.to_s + "th"
       end
-      string = "Added on: #{month} #{day}, #{year} at #{hour}:#{minutes}:#{seconds}"
-      result = [string, var]
+      string = "Added on: #{month} #{day}, #{year} at #{hour}:#{minutes}"
+      result = [string, month, day, year, hour, minutes, seconds]
     end
 
-    def edited_date_time
+    def updated_date_time(var)
+      created = created_date_time(var)
 
+      date_time = var.updated_at.localtime.to_a
+      date_time.pop
+      date_time.pop
+      date_time.pop
+      date_time.pop
+      date_time = date_time.reverse
+      
+      month = date_time[1]
+      day = date_time[2]
+      year = date_time[0].to_s
+      
+      hour = date_time[3].to_s
+      minutes = date_time[4].to_s
+      seconds = date_time[5].to_s
+
+      case month
+        when 1
+          month = "January"
+        when 2
+          month = "February"
+        when 3
+          month = "March"
+        when 4
+          month = "April"
+        when 5
+          month = "May"
+        when 6
+          month = "June"
+        when 7
+          month = "July"
+        when 8
+          month = "August"
+        when 9
+          month = "September"
+        when 10
+          month = "October"
+        when 11
+          month = "Nov"
+        when 12
+          month = "December"
+      end
+
+      case day
+        when 1
+          day = "1st"
+        when 2
+          day = "2nd"
+        when 3
+          day = "3rd"
+        when 4..31
+          day = day.to_s + "th"
+      end
+      
+      string = "Edited on: #{month} #{day}, #{year} at #{hour}:#{minutes}"
+      result = [string, month, day, year, hour, minutes, seconds]
+      if created[1] == result[1] && created[2] == result[2] && created[3] == result[3] && created[4] == result[4] && created[5] == result[5] && created[6] == result[6]
+        final = "Edited on: No edits yet" 
+      else
+        final = string
+      end
+      final
     end
 
   end
