c7b812a57d86eaedf8cc8ba002bf7c38e538e189
Cloudymolecule <baires82@gmail.com>
2020-11-26 22:50:07 -0500

    finished edit routes

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -45,11 +45,40 @@ class RollsController < ApplicationController
   end
 
   get "/rolls/:id/edit" do
+    @roll = Roll.find_by(id: params[:id])
     erb :"/rolls/edit"
   end
 
   patch "/rolls/:id" do
-    redirect to "/rolls/:id"
+    roll = Roll.find_by(id: params[:id])
+    roll.update(
+      exp_count: params[:roll][:exp_count],
+      brand: params[:roll][:brand],
+      iso: params[:roll][:iso],
+      comments: params[:roll][:comments],
+      camera_id: params[:roll][:camera_id],
+      user_id: params[:roll][:user_id],
+      updated_at: Time.now
+    )
+
+    if roll.save
+      camera = Camera.find_by(id: params[:roll][:camera_id])
+      camera.loaded = 1
+      camera.save
+      redirect to "/rolls/#{roll.id}"
+    else
+      @errors = []
+      error = roll.errors.full_messages
+      error.each do |e|
+        if e == "Camera can't be blank"
+          e = "You must select a Camera"
+        end
+        @errors << e
+      end
+      @errors = roll.errors.full_messages
+      @roll = roll
+      erb :"/rolls/edit"
+    end
   end
 
   delete "/rolls/:id/delete" do
