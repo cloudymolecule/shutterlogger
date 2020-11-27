7c6f97fb022bd17381e6cc81444875c2d959a9a8
Cloudymolecule <baires82@gmail.com>
2020-11-27 16:57:19 -0500

    added date and time conversion for views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,6 +53,68 @@ class ApplicationController < Sinatra::Base
       end
     end
 
+    def created_date_time(var) #working currently
+
+      date_time = var.created_at.localtime.to_a
+      date_time.pop
+      date_time.pop
+      date_time.pop
+      date_time.pop
+      date_time = date_time.reverse
+      
+      month = date_time[1]
+      day = date_time[2]
+      year = date_time[0].to_s
+      
+      hour = date_time[3].to_s
+      minutes = date_time[4].to_s
+      seconds = date_time[5].to_s
+
+      case month
+        when 1
+          month = "January"
+        when 2
+          month = "February"
+        when 3
+          month = "March"
+        when 4
+          month = "April"
+        when 5
+          month = "May"
+        when 6
+          month = "June"
+        when 7
+          month = "July"
+        when 8
+          month = "August"
+        when 9
+          month = "September"
+        when 10
+          month = "October"
+        when 11
+          month = "Nov"
+        when 12
+          month = "December"
+      end
+
+      case day
+        when 1
+          day = "1st"
+        when 2
+          day = "2nd"
+        when 3
+          day = "3rd"
+        when 4..31
+          day = day.to_s + "th"
+      end
+      string = "Added on: #{month} #{day}, #{year} at #{hour}:#{minutes}:#{seconds}"
+      result = [string, var]
+    end
+
+    def edited_date_time
+
+    end
+
   end
 
 end
